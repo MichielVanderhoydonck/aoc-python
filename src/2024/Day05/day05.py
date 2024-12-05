268502d17445557535ec75a56d17a72519ce20f0
Michiel <michiel.vanderhoydonck@gmail.com>
2024-12-05 18:15:18 +0100

    chore: tidy

--- src/2024/Day05/day05.py
+++ src/2024/Day05/day05.py
@@ -1,11 +1,10 @@
 def calculate(line_of_rules: list[str], line_of_pages: list[str]):
     def reorder_pages(pages, rules):
-        # Use the rules to sort the pages correctly
         dependencies = {page: set() for page in pages}
         for rule in rules:
-            l = rule.split("|")
-            if int(l[0]) in pages and int(l[1]) in pages:
-                dependencies[int(l[1])].add(int(l[0]))
+            page_number = rule.split("|")
+            if int(page_number[0]) in pages and int(page_number[1]) in pages:
+                dependencies[int(page_number[1])].add(int(page_number[0]))
         sorted_pages = sorted(pages, key=lambda x: len(dependencies[x]))
         return sorted_pages
 
@@ -16,10 +15,10 @@ def calculate(line_of_rules: list[str], line_of_pages: list[str]):
         pages = list(map(int, line.split(",")))
         check = True
 
-        for r in line_of_rules:
-            l = r.split("|")
-            if int(l[0]) in pages and int(l[1]) in pages:
-                if pages.index(int(l[0])) > pages.index(int(l[1])):
+        for rule in line_of_rules:
+            page_number = rule.split("|")
+            if int(page_number[0]) in pages and int(page_number[1]) in pages:
+                if pages.index(int(page_number[0])) > pages.index(int(page_number[1])):
                     check = False
                     break
 
@@ -66,10 +65,10 @@ if __name__ == "__main__":
     print("Part 1")
     line_of_rules, line_of_pages = read_example()
     sum_correct, sum_corrected = calculate(line_of_rules, line_of_pages)
-    print(f"Total sum of correct updates: {sum_correct}")
-    print(f"Total sum of corrected updates: {sum_corrected}")
+    print(f"  Total sum of correct updates: {sum_correct}")
+    print(f"  Total sum of corrected updates: {sum_corrected}")
     print("Part 2")
     line_of_rules, line_of_pages = read_input()
     sum_correct, sum_corrected = calculate(line_of_rules, line_of_pages)
-    print(f"Total sum of correct updates: {sum_correct}")
-    print(f"Total sum of corrected updates: {sum_corrected}")
+    print(f"  Total sum of correct updates: {sum_correct}")
+    print(f"  Total sum of corrected updates: {sum_corrected}")
