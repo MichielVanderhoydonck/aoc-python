845a7becd98d3b0ad4e812b0a2d4039dbc2d8c7b
Michiel <michiel.vanderhoydonck@gmail.com>
2025-01-05 16:16:27 +0100

    aoc: day 7

--- src/2024/Day07/day07.py
+++ src/2024/Day07/day07.py
@@ -1,37 +1,39 @@
 from itertools import product
 
+
 def solve(input: list[str]):
     total_calibrations = 0
-    
+
     for line in input:
-        result, number_string = line.split(': ')
+        result, number_string = line.split(": ")
         result = int(result)
-        numbers= list(map(int, number_string.split()))
-        
+        numbers = list(map(int, number_string.split()))
+
         # all combinations of operators for the length of numbers -1, two numbers can only be operated on once...
-        ops_comb = product(['+','*','||'], repeat=len(numbers)-1)
-        
+        ops_comb = product(["+", "*", "||"], repeat=len(numbers) - 1)
+
         for ops in ops_comb:
             if eval(numbers, ops) == result:
                 total_calibrations += result
                 break
-                
+
     return total_calibrations
 
-    
+
 def eval(numbers, operators):
     result = numbers[0]
-    
+
     for i, op in enumerate(operators):
-        if op == '+':
-            result += numbers[i+1]
-        if op == '*':            
-            result *= numbers[i+1]
-        if op == '||':            
+        if op == "+":
+            result += numbers[i + 1]
+        if op == "*":
+            result *= numbers[i + 1]
+        if op == "||":
             result = int(str(result) + str(numbers[i + 1]))
 
-    return result        
-            
+    return result
+
+
 def read_file(name: str):
     with open(name, "r") as file:
         return [line.strip() for line in file.readlines()]
